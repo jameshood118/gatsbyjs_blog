df31c3888555dd7831ce7ff98d1b7b6bc720538e
James Hood <jhood@vatcinc.com>
2020-07-12 17:54:18 -0400

    tweeak to blog post

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,9 +6,20 @@ export default function BlogPost({ data }) {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
-        <h1>{post.frontmatter.title}</h1>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      <div className="columns">
+        <div class="column is-12">
+          <div className="card">
+          <header class="card-header">
+            <h1 class="card-header-title">
+              {post.frontmatter.title}
+            </h1>
+          </header>
+            <div className="card-content">
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            </div>
+          </div>
+      
+        </div>
       </div>
     </Layout>
   )
